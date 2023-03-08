5a43c5e0a8fe7f4355302a797654879f83969152
Rafał Chomczyk <rchomczyk@icloud.com>
2023-03-08 10:11:21 +0100

    Allow for providing own component converter, in case of use modified serializers

--- src/main/java/moe/rafal/configs/serdes/SerdesKyori.java
+++ src/main/java/moe/rafal/configs/serdes/SerdesKyori.java
@@ -2,6 +2,7 @@ package moe.rafal.configs.serdes;
 
 import eu.okaeri.configs.serdes.OkaeriSerdesPack;
 import eu.okaeri.configs.serdes.SerdesRegistry;
+import moe.rafal.configs.serdes.component.converter.ComponentConverter;
 import moe.rafal.configs.serdes.component.converter.ComponentConverterLegacy;
 import moe.rafal.configs.serdes.component.converter.minimessage.ComponentConverterMiniMessage;
 import moe.rafal.configs.serdes.serializer.TitleSerializer;
@@ -10,18 +11,21 @@ import org.jetbrains.annotations.NotNull;
 
 public class SerdesKyori implements OkaeriSerdesPack {
 
-    private final boolean supportsMiniMessage;
+    private final ComponentConverter componentConverter;
+
+    public SerdesKyori(ComponentConverter componentConverter) {
+        this.componentConverter = componentConverter;
+    }
 
     public SerdesKyori(boolean supportsMiniMessage) {
-        this.supportsMiniMessage = supportsMiniMessage;
+        this(supportsMiniMessage
+            ? new ComponentConverterMiniMessage()
+            : new ComponentConverterLegacy());
     }
 
     @Override
     public void register(@NotNull SerdesRegistry registry) {
-        registry.register(new StringToComponentTransformer(supportsMiniMessage
-            ? new ComponentConverterMiniMessage()
-            : new ComponentConverterLegacy()));
-
+        registry.register(new StringToComponentTransformer(componentConverter));
         registry.register(new TitleSerializer());
         registry.register(new TitleSerializer.TimesSerializer());
     }
