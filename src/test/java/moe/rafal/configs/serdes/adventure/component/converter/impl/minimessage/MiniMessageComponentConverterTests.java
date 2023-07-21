594b597315890bc404ec7197d9398849a3f73f5b
Rafał Chomczyk <rchomczyk@icloud.com>
2023-07-21 15:44:27 +0200

    Fix invalid access scope

--- src/test/java/moe/rafal/configs/serdes/adventure/component/converter/impl/minimessage/MiniMessageComponentConverterTests.java
+++ src/test/java/moe/rafal/configs/serdes/adventure/component/converter/impl/minimessage/MiniMessageComponentConverterTests.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class MiniMessageComponentConverterTests {
+class MiniMessageComponentConverterTests {
 
     private final ComponentConverter componentConverter = new MiniMessageComponentConverter();
 
